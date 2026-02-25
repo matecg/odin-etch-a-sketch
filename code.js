8978536c9339ec27474413f31c5ca08296ea114b
Gody <250132528+matecg@users.noreply.github.com>
2026-02-25 08:58:09 -0300

    Add drawing logic to grid tiles

--- code.js
+++ code.js
@@ -3,8 +3,15 @@ const container = document.querySelector(".grid-container");
 const sizeInput = document.querySelector("#row-size");
 const setBtn = document.querySelector(".set-btn");
 
+let isDrawing = false;
+
+document.querySelector(".main").addEventListener('mouseup', () => {
+    isDrawing = false;
+});
 container.style.minHeight = `${CONTAINER_SIZE}px`;
 container.style.minWidth = `${CONTAINER_SIZE}px`;
+container.addEventListener('mousedown', () => isDrawing = true);
+container.addEventListener('mouseup', () => isDrawing = false);
 setBtn.addEventListener('click', (e) => {
     e.preventDefault();
     renderGrid(Number(sizeInput.value));
@@ -23,6 +30,14 @@ function renderGrid(rowSize)
         grid.classList.add("square");
         grid.style.height = `${gridSize}px`;
         grid.style.flexBasis = `${gridSize}px`;
+        grid.addEventListener('mouseover', (e) => {
+            if (!isDrawing) return;
+
+            e.target.style.backgroundColor = e.ctrlKey ? 'white' : 'black';
+        });
+        grid.addEventListener('mousedown', (e) => {
+            e.target.style.backgroundColor = e.ctrlKey ? 'white' : 'black';
+        })
         gridBg.appendChild(grid);
     }
     container.appendChild(gridBg);
